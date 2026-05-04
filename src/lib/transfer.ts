59aded2b56a1beb27ad8f8ae96fb84fde52479ca
[dyad] <alextavares618@gmail.com>
2026-05-04 11:06:13 -0300

    [dyad] wrote 3 file(s)

--- src/lib/transfer.ts
+++ src/lib/transfer.ts
@@ -1,5 +1,1 @@
-export async function recompute_invoice_total(p_invoice: string) {
-  const { data, error } = await supabase.rpc("recompute_invoice_total", { p_invoice });
-  if (error) throw error;
-  return data;
-}
+Add `export` to the transferFunds function declaration.
